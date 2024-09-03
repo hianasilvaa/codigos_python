c9a6da5d065b35676b95c44a825bf285ece35305
Hiana <hianasilvaa21@gmail.com>
2024-09-03 12:03:00 -0300

    Calcular_volume.py

---
+++ Atividade 1/calcular_volume.py
@@ -0,0 +1,9 @@
+raio = float(input("informe o raio:"))
+altura = float(input("informe a altura: "))
+volume =((RAIO**2)*ALTURA*3.14159)
+
+# Calcula o volume
+volume = calcular_volume_lata(raio*altura)
+
+ # Exibe o resultado
+Print(f"O volum da lata é: {volume:.2f}")#exibir resultado
