9453f943ac8e80890df4cf91201fbd0cd2a94867
Hiana <hianasilvaa21@gmail.com>
2024-09-03 09:49:03 -0300

    Calculando média com python

--- exemplo2.py
+++ exemplo2.py
@@ -2,4 +2,14 @@ nome = input("informe seu nome: ")
 end = input("informe seu endereço: ")
 idade = input("informe sua idade: ")
 
-print (nome,end,idade)
+# Exibindo dados das variáveis
+#print (nome,end,idade)
+
+#1ª forma de concatenação
+print("\nOlá",nome, "seu endereço é",end,"sua idade é",idade)
+
+#2ª forma de concatenação
+print("\nOlá {} seu endereço é {} e sua idade é {}".format(nome,end,idade))
+
+#3ª forma de concatenação - f string
+print(f"Bem vindo {nome}, você mora na {end} e tem {idade} anos")
---
+++ exemplo3.py
@@ -0,0 +1,7 @@
+nota1 = float(input("informe sua 1ª nota: "))# estamos convertendo os valores textuais em valores deimais.
+nota2 = float( input("informe sua 2ª nota: "))
+nota3 = float(input("informe sua 3ª nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"sua média é {media:.2f}")# .2f significa que teremos 2 casas decimais após a vírgula
